e28c6d963e76a579c532c9db230e131f2bba1e6d
Antonio Miras <antonio.salvador@alten.es>
2022-09-08 10:47:21 +0200

    HamburguerHeaven challenge

---
+++ UWP-023/HamburguerHeaven/HamburguerHeaven/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace HamburguerHeaven
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ContentFrame.Navigate(typeof(Financial));
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FinancialListBoxItem.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(Financial));
+                SectionTitle.Text = "Financial";
+                BackButton.Visibility = Visibility.Collapsed;
+            }
+            else if (FoodListBoxItem.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(Food));
+                SectionTitle.Text = "Food";
+                BackButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            IconsListBox.SelectedItem = FinancialListBoxItem;
+        }
+    }
+}
