5d6180c23610294c766395d3f7ace7727dd20534
Antonio Miras <antonio.salvador@alten.es>
2022-10-26 07:49:43 +0200

    Keep two separate folders for Uwp and WinAppSdk examples

---
+++ WinAppSdk/WinUi3-020/ControlsExamplePart1/ControlsExamplePart1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ControlsExamplePart1
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MyCheckBox_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            CheckBoxResultTextBlock.Text = MyCheckBox.IsChecked.ToString();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButtonTextBlock.Text = (bool)YesRadioButton.IsChecked ? "Yes" : "No";
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBoxResultTextBlock == null) return;
+
+            var combo = (ComboBox)sender;
+            var item = (ComboBoxItem)combo.SelectedItem;
+            ComboBoxResultTextBlock.Text = item.Content.ToString();
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItems = MyListBox.Items.Cast<ListBoxItem>()
+                                  .Where(p => p.IsSelected)
+                                    .Select(t => t.Content.ToString())
+                                      .ToArray();
+
+            ListBoxResultTextBlock.Text = string.Join(", ", selectedItems);
+
+        }
+
+        private void MyToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButtonResultTextBlock.Text = MyToggleButton.IsChecked.ToString();
+        }
+
+    }
+}
