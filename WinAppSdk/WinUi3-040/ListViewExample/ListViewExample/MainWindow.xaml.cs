b9b31b14a1af87a1a62dfa1c6957c5c1eddab54e
Antonio Miras <antonio.salvador@alten.es>
2022-11-11 15:31:40 +0100

    Chapter 040

---
+++ WinAppSdk/WinUi3-040/ListViewExample/ListViewExample/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ListViewExample.Model;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using static System.Reflection.Metadata.BlobBuilder;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ListViewExample
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private List<Book> Books;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            Books = BookManager.GetBooks();
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var book = (Book)e.ClickedItem;
+            ResultTextBlock.Text = "You Selected: " + book.Title;
+        }
+    }
+}
