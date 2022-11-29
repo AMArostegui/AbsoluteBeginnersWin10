631b4f86aa684af573571bb3948f05c0974287e5
Antonio Miras <antonio.salvador@alten.es>
2022-11-29 16:56:52 +0100

    Chapters 014, 042

---
+++ WinAppSdk/WinUi3-042/UserCtrlData/UserCtrlData/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UserCtrlData.Models;
+using Windows.ApplicationModel.Contacts;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace UserCtrlData
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private List<Icon> Icons;
+
+        private ObservableCollection<Contact2> Contacts;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            Icons = new List<Icon>();
+            Icons.Add(new Icon { IconPath = "Assets/male-01.png" });
+            Icons.Add(new Icon { IconPath = "Assets/male-02.png" });
+            Icons.Add(new Icon { IconPath = "Assets/male-03.png" });
+            Icons.Add(new Icon { IconPath = "Assets/female-01.png" });
+            Icons.Add(new Icon { IconPath = "Assets/female-02.png" });
+            Icons.Add(new Icon { IconPath = "Assets/female-03.png" });
+
+            Contacts = new ObservableCollection<Contact2>();
+
+        }
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            string avatar = ((Icon)AvatarComboBox.SelectedValue).IconPath;
+            Contacts.Add(new Contact2 { FirstName = FirstNameTextBox.Text, LastName = LastNameTextBox.Text, AvatarPath = avatar });
+
+            FirstNameTextBox.Text = "";
+            LastNameTextBox.Text = "";
+            AvatarComboBox.SelectedIndex = -1;
+
+            FirstNameTextBox.Focus(FocusState.Programmatic);
+        }
+    }
+}
